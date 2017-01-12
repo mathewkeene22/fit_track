daf2006f6af0a4170b93cc313b14060424a5cd6e
Mathew Keene <mathewkeene22@gmail.com>
2017-01-11 19:11:52 -0800

    update capybara gem to avoid rack errors

--- spec/features/creating_home_page_spec.rb
+++ spec/features/creating_home_page_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.feature "creating home page" do
 	scenario do
 		visit visit '/'
-		
-		expect(page).to have_content("Athelets Den")
+
+		expect(page).to have_content("Athlets Den")
 		expect(page).to have_content("Home")
 		expect(page).to have_content("Workout Lounge")
 	end
