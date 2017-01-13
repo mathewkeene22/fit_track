dd2ccd0d69722dc08d7139cbaab8585529171c61
Mathew Keene <mathewkeene22@gmail.com>
2017-01-12 17:55:06 -0800

    cleaned up user sign in spec

--- spec/features/user_sign_out_spec.rb
+++ spec/features/user_sign_out_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.feature "Users sign-in"  do
 	before do
 		@john = User.create!(email: "john@example.com", password: "password")
-	end
 
-	scenario "with valid credentails" do
 		visit "/"
-		click_link "Sign in"
+		click_link "Sign In"
 		fill_in "Email", with: @john.email
 		fill_in "Password", with: @john.password
 		click_button "Log in"
+	end
 
-		expect(page).to have_content("Signed in successfully.")
-		expect(page).to have_content("Signed in as: " + @john.email)
+	scenario "signs in successfully and signs out" do
+		click_link "Sign Out"
+		expect(page).to have_content("Signed out successfully.")
 	end
 end
