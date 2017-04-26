896319e8b93d7ba01f730c7c0f3716dce69052b2
Mathew Keene <mathewkeene22@gmail.com>
2017-04-26 16:41:44 -0700

    added morris and raphael, workouts now have charts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery-ui/widgets/datepicker
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+// require turbolinks
 //= require_tree .
+//= require raphael-min
+//= require morris
